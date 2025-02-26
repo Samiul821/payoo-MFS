43dacda75f7dc565f70a8513a9984668b3795481
Samiul821 <smsamiul821@gmail.com>
2025-02-26 09:50:56 +0600

    addMoney.js file function update

---
+++ script/addMoney.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const amount = document.getElementById("amount").value;
+    const convertedAmount = parseFloat(amount);
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+    const mainBalence = document.getElementById("main-balence").innerText;
+    const convertedMainBalenc = parseFloat(mainBalence);
+
+    if (amount && pin) {
+      if (convertedPin === 2255) {
+        const sum = convertedMainBalenc + convertedAmount;
+        document.getElementById("main-balence").innerText = sum;
+      } else {
+        alert("enter valid pin");
+      }
+    } else {
+      alert("enter amount");
+    }
+  });
