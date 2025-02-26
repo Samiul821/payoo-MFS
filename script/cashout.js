5f5c46c3009e6e56a06297401dd4cc0559625a2e
Samiul821 <smsamiul821@gmail.com>
2025-02-27 00:00:35 +0600

    implement cashout using function

--- script/cashout.js
+++ script/cashout.js
@@ -1,18 +1,39 @@
+// document
+//   .getElementById("withdraw-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const pin = document.getElementById("cashout-pin").value;
+//     const convertedPin = parseInt(pin);
+//     const cashoutAmount = document.getElementById("cashout-amount").value;
+//     const convertedCashoutAmount = parseFloat(cashoutAmount);
+//     const mainBalence = document.getElementById('main-balence').innerText;
+//     const convertedMainBalenc = parseFloat(mainBalence);
+//     if(convertedPin === 2255){
+//        const sum = convertedMainBalenc - convertedCashoutAmount;
+//        document.getElementById('main-balence').innerText = sum;
+//     }
+//     else{
+//         alert('enter valid pin');
+//     }
+//   });
+
 document
   .getElementById("withdraw-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const pin = document.getElementById("cashout-pin").value;
-    const convertedPin = parseInt(pin);
-    const cashoutAmount = document.getElementById("cashout-amount").value;
-    const convertedCashoutAmount = parseFloat(cashoutAmount);
-    const mainBalence = document.getElementById('main-balence').innerText;
-    const convertedMainBalenc = parseFloat(mainBalence);
-    if(convertedPin === 2255){
-       const sum = convertedMainBalenc - convertedCashoutAmount;
-       document.getElementById('main-balence').innerText = sum;
-    } 
-    else{
-        alert('enter valid pin');
+    const agentNumber = document.getElementById("agent-number").value;
+    const amount = getInputValueId("cashout-amount");
+    const pinNumber = getInputValueId("cashout-pin");
+    const mainBalence = getInnerTextId("main-balence");
+
+    if (agentNumber.length === 11) {
+      if (pinNumber === 2255) {
+        const sum = mainBalence - amount;
+        setInnerTextByIdandValue("main-balence", sum);
+      } else {
+        alert("pin number not valid");
+      }
+    } else {
+      alert("agent number not valid");
     }
   });
